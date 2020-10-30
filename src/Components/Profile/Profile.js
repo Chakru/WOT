fe9d98f7d5e9fd0d3f2e52305cf05c6ff1b07e94
Abhishek Chakravarty <abhishek.chakravarthy@ellucian.com>
2020-10-30 21:22:24 +0530

    Dummy profile page

---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Profile.css';
+function Profile() {
+  return <div>User Profile</div>;
+}
+
+export default Profile;
--- src/Components/Router/LayoutRouter.js
+++ src/Components/Router/LayoutRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from '../Login/Login';
 import OTPVerify from '../Login/OTPVerify';
+import Profile from '../Profile/Profile';
 import Signup from '../Signup/Signup';
 function LayoutRouter() {
   return (
@@ -11,6 +12,7 @@ function LayoutRouter() {
           <Route path="/" exact component={Login} />
           <Route path="/login" component={OTPVerify} />
           <Route path="/register" component={Signup} />
+          <Route path="/user" component={Profile} />
         </Switch>
       </Router>
     </div>
