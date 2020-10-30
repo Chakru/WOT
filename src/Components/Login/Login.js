478c1660a54303a28dbe19d0ae2bd970bc85d91e
Abhishek Chakravarty <abhishek.chakravarthy@ellucian.com>
2020-10-30 11:15:09 +0530

    #5 Login flow modification and dead code removal

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
-import OTPVerify from './OTPVerify';
 
 function Login() {
   const [user, setUser] = useState({ phoneNumber: '' });
 
   const loginHandler = e => {
     e.preventDefault();
-    console.log(user.phoneNumber);
+  };
+
+  let userNumber = {
+    phoneNumber: user.phoneNumber,
   };
 
   return (
     <>
       <div className="login">
         <div className="loginContainer">
-          {/* <h2 className="heading">Women of Today</h2> */}
-          <form onSubmit={loginHandler}>
-            <label>Phone Number</label>
+          <h2 className="heading">Women of Today</h2>
+          <form onClick={loginHandler}>
+            <p>Please enter your mobile number to get started</p>
             <input
               name="phoneNumber"
-              placeholder="Phone #"
-              pattern="[0-9]{5}-[0-9]{5}"
+              placeholder="Mobile Number"
+              pattern="[6-9]{3}-[0-9]{3}-[0-9]{4}"
               type="tel"
               minLength="10"
               maxLength="10"
               value={user.phoneNumber}
               onChange={e => setUser({ ...user, phoneNumber: e.target.value })}
-              required="'required'"
+              required
             />
-            {/* error != '' ? <div className="errorMsg">{error}</div> : '' */}
             <div className="btnContainer">
               <Link to="/login">
                 <button disabled={user.phoneNumber.length < 10}>Get OTP</button>
